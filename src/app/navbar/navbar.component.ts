325de7764871eea6627534e9a9458de4f6c119a3
jagdishpatil19 <patiljagdish471@gmail.com>
2024-08-16 15:15:22 +0530

    drop dwon change

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,50 +7,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  productDropdown:boolean=false
-  flavourDrop:boolean=false
-  userDrop:boolean=false
+  
   cartHide:boolean=true
  
 
   constructor(private router:Router){}
-  productDrop(){
-    this.productDropdown=!this.productDropdown
-  
-    if(this.productDropdown==true){
-      this.flavourDrop=false
-      this.userDrop=false
-    }
-  }
-
-  flavourDropdown(){
-    this.flavourDrop=!this.flavourDrop
-    if(this.flavourDrop==true){
-      this.productDropdown=false
-      this.userDrop=false
-    }
-  }
-  userDropdown(){
-    this.userDrop=!this.userDrop
-    if(this.userDrop==true){
-      this.flavourDrop=false
-      this.productDropdown=false
-    }
-  }
+   
   cartToggle(){
  this.cartHide=false
   }
-  hideDropDwoun(){
-    this.productDropdown=false
-  }
-  dropDowncancle(){
-    if(this.productDropdown==true || this.flavourDrop==true ||  this.userDrop==true){
-      this.productDropdown=false
-      this.flavourDrop=false
-      this.userDrop=false
-
-    }
-  }
+ 
 
 
   // search method impliment
